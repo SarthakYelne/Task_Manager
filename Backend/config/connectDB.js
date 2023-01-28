73c14bbb657b59ca25ee232445f1c60ce0f42480
Sarthak Yelne <sarthakyelne2003@gmail.com>
2023-01-28 14:48:25 +0530

    Create task added

---
+++ Backend/config/connectDB.js
@@ -0,0 +1,15 @@
+
+
+
+const connectDB = async () => {
+    try{
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`Mongoose Connected`);
+    }catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB
---
+++ Backend/model/task_models.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const taskSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please Add Task"]
+        },
+        complete: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Task = mongoose.model("Task", taskSchema)
+
+ module.exports = Task;
